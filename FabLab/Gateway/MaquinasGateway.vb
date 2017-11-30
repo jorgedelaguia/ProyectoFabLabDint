88e4b81ba155582f4b83c9153bca0cefb75c5cbb
Teodor <Teodor.vladislavov@iesdoctorbalmis.com>
2017-11-30 13:29:52 +0100

    Añade maquinas gateway

---
+++ FabLab/Gateway/MaquinasGateway.vb
@@ -0,0 +1,52 @@
+﻿Imports System.Data.SqlClient
+
+Public Class MaquinasGateway
+
+    Private conexion As SqlConnection
+    Private comando As SqlCommand
+
+    Public Sub New(ByRef cadenaConexion As String)
+        conexion = New SqlConnection(cadenaConexion)
+        comando = New SqlCommand()
+        comando.Connection = conexion
+    End Sub
+
+    Public Function Actualizar(id As Integer, modelo As String, precio_hora As Decimal, fecha_compra As Date, telefono_sat As String, tipo As Integer, descripcion As String, caracteristicas As String) As Integer
+
+        Dim filas As Integer
+        'Creamos la sentencia SQL de actualizaciónn
+        Dim consulta As String = String.Format("UPDATE Fablab SET modelo='{0}',precio_hora='{1}',fecha_compra='{2}',telefono_sat='{3}',tipo='{4}', descripcion='{5}',caracteristicas='{6}' WHERE id={7})", modelo, precio_hora, fecha_compra, fecha_compra, telefono_sat, tipo, descripcion, caracteristicas, id)
+
+        'Validamos los datos
+        If id = 0 Then
+            Throw New ArgumentException("El identificador no puede ser 0")
+        End If
+
+        If modelo = "" Or modelo Is Nothing Then
+            Throw New ArgumentException("El modelo no puede estar vacío")
+        End If
+
+
+        Dim er As New System.Text.RegularExpressions.Regex("[0-9]{9}")
+        If er.IsMatch(telefono_sat) Then
+            Throw New ArgumentException("El teléfono debe contener 9 dígitos")
+        End If
+        'Ejecutamos la consulta
+        Try
+            conexion.Open()
+            comando.CommandText = consulta
+            filas = comando.ExecuteNonQuery()
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
+
+        'Devolvemos el número de filas afectadas
+        Return filas
+
+    End Function
+
+End Class
